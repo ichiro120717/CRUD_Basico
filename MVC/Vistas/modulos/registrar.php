5de34b6713963972a01528046a2c449d265313cc
ThonyVizca99 <anthonyvizca@gmail.com>
2020-07-23 07:14:58 -0400

    Extensión de archivos .html cambiados a .php

--- MVC/Vistas/modulos/registrar.php
+++ MVC/Vistas/modulos/registrar.php
@@ -1,27 +1,4 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-	<meta charset="utf-8">
-	<title>CRUD</title>
 
-	<link rel="stylesheet" type="text/css" href="css/estilos.css">
-
-</head>
-
-<body>
-
-<nav>
-	<ul>
-		
-		<li><a href="index.html">Ingresar</a></li>
-		<li><a href="registrar.html">Registrar</a></li>
-		<li><a href="empleados.html">Empleados</a></li>
-		<li><a href="salir.html">Salir</a></li>
-
-	</ul>
-</nav>
-
-<section>
 
 	<br>
 	<h1>REGISTRAR UN EMPLEADO</h1>
@@ -41,9 +18,3 @@
 		<input type="submit" value="Registrar">
 
 	</form>
-
-</section>
-	
-</body>
-
-</html>
