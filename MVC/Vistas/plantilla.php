aafd893220181ac38882d415daf5da9cd7d0f9d6
ThonyVizca99 <anthonyvizca@gmail.com>
2020-07-23 06:46:01 -0400

    Creación de archivo menu.php dentro de la carpeta modulos

--- MVC/Vistas/plantilla.php
+++ MVC/Vistas/plantilla.php
@@ -10,17 +10,6 @@
 
 <body>
 
-<nav>
-	<ul>
-		
-		<li><a href="index.html">Ingresar</a></li>
-		<li><a href="registrar.html">Registrar</a></li>
-		<li><a href="empleados.html">Empleados</a></li>
-		<li><a href="salir.html">Salir</a></li>
-
-	</ul>
-</nav>
-
 <section>
 
 	<br>
